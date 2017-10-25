2290afda8545363b6ed9ed9e4ffbb559a2bda4b8
FlipClown <sourmento@gmail.com>
2017-10-25 19:37:14 -0400

    Add files via upload

---
+++ LIRIBOT/keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: "IyuhhOe1umktcNi0TrsJpnPYG",
+  consumer_secret: "6rdmE4GeaMZH7Q9xzxywG1QDfoTQTqRwFOqICBkSoxGwGb1rNo",
+  access_token_key: "922630045088256000-eTX6iSi4dRsvzgZAMOkezNAwmYhYWcM",
+  access_token_secret: "OwHR8XYUY6noZxpsIOasYyImeRtzOqq97cXSjxvy6awkl",
+}
+
+module.exports = twitterKeys;
